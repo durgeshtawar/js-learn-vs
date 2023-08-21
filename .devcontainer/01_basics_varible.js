8983242e2a7892725321905998b7595a210d12a1
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-21 18:42:00 +0000

    02_datatypes

---
+++ .devcontainer/01_basics_varible.js
@@ -0,0 +1,20 @@
+// const accountId = 144553
+// let accountEmail = "aditya56@gmail.com"
+// var passowrd = "12345";
+// accountCity = "khndawa"
+
+
+
+// // accountId = 2
+// accountEmail = "hs@accountEmail.com"
+// accountCity = "bangaluru"
+
+// console.log(accountId);
+// /*
+// sndjsn its a comment 
+// perfer not to use to var because of eesue in blcok and function scop 
+// */
+
+// console.table(accountEmail, accountId, passowrd);
+
+
---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+alert(3 + 3)//we are using node js , not browser 
+//console.log(3+3); console.log("adity") //bad habit code not readable 
+
+
+let name = "aditya"
+let age = 18 
+let isLoggedin = fals;
+
+//number --> 2 to power 53 range 
+//big int big number inc ase use 
+//string --> ""
+//boolean = true false 
+//null --> standalone value 
+//undefine --> 
+//symbol in rect we use that in  uniq 
+
+//object 
+console.log(typeof "aditya");
+//type of null its a object
