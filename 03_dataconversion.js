e2cdc334c1b06ec50c6db459c6d109501999fdd1
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-21 18:54:46 +0000

    03_dataconversion

---
+++ 03_dataconversion.js
@@ -0,0 +1,12 @@
+let score = true
+console.log(typeof score);
+
+let islogin =  1
+let boolisLogin = Boolean(islogin)
+console.log(boolisLogin);
+
+
+let some = 33
+
+let stringnum = String(some)
+console.log(  typeof stringnum);
