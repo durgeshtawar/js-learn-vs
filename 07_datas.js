1e0d26e486f851fda5d735dac02bf9801651ee1f
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-22 19:54:42 +0000

    07_dates

---
+++ 07_datas.js
@@ -0,0 +1,18 @@
+//dates 
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString);
+console.log(mydate.toLocaleDateString());
+console.log(typeof mydate);//its a object 
+
+let mycretaed = new Date(2023, 0, 23)
+console.log(mycretaed.toDateString());
+let mytimestamps = Date.now();
+console.log(mytimestamps);
+
+console.log(math.floor(Date.now()/1000));
+
+//use mdn more explore
+
+newDate.toLocalstring('defult', {weekday: "long
+"})
