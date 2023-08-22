3bbfc4a859ae1f2527ff4d7059f540c1872fc157
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-22 17:35:09 +0000

    datatypes_summry

---
+++ datatypes_summry.js
@@ -0,0 +1,32 @@
+//primitive  data types  7 
+
+/*
+string number boolean , null, undifined symbol Big int 
+const score = 100
+const scorevalue = 100.3 int 
+
+const id = Symbol('123)
+const anotherid = Symbo('123) //diffrent value 
+const bigNumber = 34355465664//big int 
+
+
+const islogedin = false;bool
+const outside = null 
+letuserEmai; undifined
+
+// REference type or non primitive
+Arrays objects Functions 
+const heros = {"shakti", "naagrj", "doga"};
+let myObj = {
+    name : "Hitesh",
+    age: 22, 
+
+}
+*/
+
+const myFunction= function(){
+    console.log("Hello , world");
+
+}
+console.log(typeof myFunction);//check data types non primitive data type is object all non primitive 
+//ecma 
