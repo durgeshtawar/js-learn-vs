b5a064022723a8ad06083663fb2ed735f2cdd020
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-22 17:20:54 +0000

    04_comaridion.js

---
+++ 04_comarision.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+
+// console.log(2+2)
+// (2-2)
+// (2/2)
+// (s*s)
+// (a%b)
+ // === stricly check
+ console.log("2" ===  2)
+
+ console.log( null > 0);
+ console.log(null == 0)
+ console.log(null >= 0);
+
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);//
+
+ //avoid that type comprasion
+ 
