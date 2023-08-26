5a209673f8b17268f6ad0a2602e53016ae9d6e92
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-26 05:45:47 +0000

    jason_object

---
+++ destructureobj.js
@@ -0,0 +1,32 @@
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    instructure:"aditya"
+}
+
+const {instructure} = course//de structure 
+console.log(instructure);
+
+//react basic method 
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "adtiya")
+
+//apis in obj apan kam kisi ke sar par dalana its a api's
+//values ati hai backend se and api kese structure kese karti hai 
+
+{
+    "name": "hitesh",//key and value string 
+    "courseName": "js in hinsi",
+    "price":"999"
+}
+//api are given into array formate 
+[
+    {},
+    {},
+    {}
+
+]
+   
