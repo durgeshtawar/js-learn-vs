8f9e946859eb4332db2d78b8a26feb87c0ca016b
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-08-28 11:38:51 +0000

    04_slow/switch/map/reduce/filter/loops.js

---
+++ 04_iteration.js/05_loopin Array.js
@@ -0,0 +1,61 @@
+const coding = ["js", "c", "C++", "java"]
+// coding.forEach( function (item) {//call back function
+//     console.log(item);
+// })
+
+
+// coding.forEach((item) =>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach((item, index, arr) =>{
+//     console.log(item, index, arr);
+// })
+
+
+
+// const myCoding =[
+//     {
+//         languge: "javascript",
+//         languageFile: "js"
+//     },
+//     {
+//         lnguage:"java",
+//         languageFIle: "javascript"
+//     },
+//     {
+//         language: "CPP",
+//         LanguageFile: "C++"
+//     },
+// ]
+
+// myCoding.forEach((item) =>{
+//     //console.log(item.languageFIle);
+// })
+
+const MySkills= [
+    {
+        skils: "Web dev",
+        exprience: "2year"
+    },
+    {
+        skils: "DSA",
+        exprience: "8month"
+    },
+    {
+        skils: "system design",
+        exprience: "4Month"
+    },
+]
+
+MySkills.forEach((item) => {
+    console.log(item.skils);
+})
+
+
